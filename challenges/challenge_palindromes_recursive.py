ed6b19cd429502d33bf7ef8bad728871990c7c46
Gabriel Galera <gabgalera@hotmail.com>
2023-06-01 18:29:58 -0300

    feat: req3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if low_index >= high_index:
+        return True
+    else:
+        return (
+            is_palindrome_recursive(word, low_index + 1, high_index - 1)
+            and word[low_index] == word[high_index]
+        )
+
+
+print(is_palindrome_recursive("REVIVER", 0, 6))
