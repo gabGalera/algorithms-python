86eea2a6dc92a8bf00bb1865f3532a8138db484a
Gabriel Galera <gabgalera@hotmail.com>
2023-06-01 17:59:22 -0300

    feat: req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    for index in range(len(permanence_period)):
+        if (
+            not type(permanence_period[index][0]) is int
+            or not type(permanence_period[index][1]) is int
+        ):
+            return None
+        if target_time in range(
+            permanence_period[index][0],
+            permanence_period[index][1] + 1,
+        ):
+            count += 1
+    if count:
+        return count
