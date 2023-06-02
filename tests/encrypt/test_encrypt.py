5f8b15fde16b1734ca9e87b26213523bf7c170f9
Gabriel Galera <gabgalera@hotmail.com>
2023-06-02 14:58:51 -0300

    vendo se cola

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
-def test_encrypt_message():
-    pass
+def test_if_not_string_throws_an_error():
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(10, 10)
+
+
+def test_if_not_int_throws_an_error():
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("10", "10")
+
+
+def test_if_key_not_in_range():
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Xablau", -10)
+
+
+def test_if_it_is_odd():
+    assert encrypt_message("Xablau", 3) == "baX_ual"
+
+
+def test_if_it_is_pair():
+    assert encrypt_message("Xablau", 2) == "ualb_aX"
