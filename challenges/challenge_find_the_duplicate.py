438839be453951f6c36c6a0063be87fb4bb08900
Gabriel Galera <gabgalera@hotmail.com>
2023-06-06 10:32:20 -0300

    feat: Req6

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -14,34 +14,3 @@ def find_duplicate(nums):
             return nums[position]
 
     return False
-
-
-def merge_sort(nums, start=0, end=None):
-    if end is None:
-        end = len(nums)
-    if (end - start) > 1:
-        mid = (start + end) // 2
-        merge_sort(nums, start, mid)
-        merge_sort(nums, mid, end)
-        merge(nums, start, mid, end)
-
-
-def merge(nums, start, mid, end):
-    left = nums[start:mid]
-    right = nums[mid:end]
-
-    left_index, right_index = 0, 0
-
-    for index in range(start, end):
-        if left_index >= len(left):
-            nums[index] = right[right_index]
-            right_index += 1
-        elif right_index >= len(right):
-            nums[index] = left[left_index]
-            left_index + 1
-        elif left[left_index] < right[right_index]:
-            nums[index] = left[left_index]
-            left_index += 1
-        else:
-            nums[index] = right[right_index]
-            right_index += 1
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    length = len(word)
+    if not word:
+        return False
+    for letter_index in range(0, length // 2):
+        start = word[letter_index]
+        end = word[length - letter_index - 1]
+        if start != end:
+            return False
+    return True
+
+
+print(is_palindrome_iterative(""))
